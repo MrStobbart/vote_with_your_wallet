a6d95c2cbbbd7300a57808e73d3a631ff8e056b2
Marek Meyer <marek_meyer@outlook.com>
2018-03-14 16:55:51 +0000

    Add project files.

---
+++ vote_with_your_wallet/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace vote_with_your_wallet
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ vote_with_your_wallet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vote_with_your_wallet.MvcApplication" Language="C#" %>

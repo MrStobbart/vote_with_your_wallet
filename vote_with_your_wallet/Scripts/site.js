a62ba58b6bcf2c799e4d62e36eeab9be76e29d4d
Marek Meyer <marek_meyer@outlook.com>
2018-03-16 14:05:08 +0000

    * Added Contact section * Added Information section * Some style fixes

--- vote_with_your_wallet/Scripts/site.js
+++ vote_with_your_wallet/Scripts/site.js
@@ -1,76 +1,78 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 57)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 57)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 57
-  });
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 57
+    });
 
-  // Collapse Navbar for different styled navbar when scrolling down
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink"); 
-    }
-  };
+    // Collapse Navbar for different styled navbar when scrolling down
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink"); 
+        }
+    };
 
-  // Initial call of navbarCollapse
-  navbarCollapse();
+    // Initial call of navbarCollapse
+    navbarCollapse();
 
-  // Call navbarCollapse when page is scrolled
-  $(window).scroll(navbarCollapse);
+    // Call navbarCollapse when page is scrolled
+    $(window).scroll(navbarCollapse);
 
 
-  $('#submit-login').on('click', function () {
-      console.log('submit login')
-      $('#login-toggle').text('Logout')
-      $('#sign-up').addClass('d-none')
-  });
+    // Change header when user is logged in
+    $('#submit-login').on('click', function () {
+        console.log('submit login')
+        $('#login-toggle').text('Logout')
+        $('#sign-up').addClass('d-none')
+    });
 
-  $('#login-toggle').on('click', function () {
+    // Reset navbar on logout / show modal on login
+    $('#login-toggle').on('click', function () {
 
-      if ($('#login-toggle').text() === 'Login') {
-          $('#login-modal').modal('show');
-      } else {
-          $('#login-toggle').text('Login')
-          $('#sign-up').removeClass('d-none')
-      }
+        if ($('#login-toggle').text() === 'Login') {
+            $('#login-modal').modal('show');
+        } else {
+            $('#login-toggle').text('Login')
+            $('#sign-up').removeClass('d-none')
+        }
       
-  });
+    });
 
-// Update supporter when cause is supported 
-  $('.support-button').on('click', function () {
-      var targetBaseId = $(this).attr('for')
+    // Update supporter when cause is supported 
+    $('.support-button').on('click', function () {
+        var targetBaseId = $(this).attr('for')
 
-      // Update supporter count
-      var targetCountIt = targetBaseId + '-cnt';
-      var currentCount = $('#' + targetCountIt).text();
-      var newCount = parseInt(currentCount) + 1;
-      $('#' + targetCountIt).text(newCount)
+        // Update supporter count
+        var targetCountIt = targetBaseId + '-cnt';
+        var currentCount = $('#' + targetCountIt).text();
+        var newCount = parseInt(currentCount) + 1;
+        $('#' + targetCountIt).text(newCount)
 
-      // Add name to the beginning of supporters (static atm)
-      $('#' + targetBaseId + '-names > tbody > tr:first').before('<tr><td scope= "row" >Peter Barclay</td></tr>');
-  });
+        // Add name to the beginning of supporters (static atm)
+        $('#' + targetBaseId + '-names > tbody > tr:first').before('<tr><td scope= "row" >Peter Barclay</td></tr>');
+    });
 
 })(jQuery); // End of use strict

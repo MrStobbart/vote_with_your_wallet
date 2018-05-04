c622a2bd70eafefc61b167f68ec74b2af422625f
Marek Meyer <marek_meyer@outlook.com>
2018-05-04 20:59:45 +0100

    Added share buttons for reddit and twitter

--- vote_with_your_wallet/Scripts/site.js
+++ vote_with_your_wallet/Scripts/site.js
@@ -129,6 +129,7 @@
         })
     })
 
+
     window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = $('.needs-validation'); 
